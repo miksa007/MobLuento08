03781418a99389ed0fa62eefc9040bfcb0aa6652
miksa <miksa007@gmail.com>
2017-03-13 10:49:34 +0200

    luento 8 _ firebase testailua toDo aplikaatiolla

---
+++ app/src/main/java/fi/tut/saari5/todoapplication/Task.java
@@ -0,0 +1,15 @@
+package fi.tut.saari5.todoapplication;
+
+/**
+ * Created by miksa on 3/10/17.
+ */
+public class Task {
+    private String task;
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
